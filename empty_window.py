e9d041749b9e1bd8dc93ec3331e5215d5ab568e0
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-03-10 00:09:42 +0100

    FEAT: Logic to move the Ship [DRAFT]

--- empty_window.py
+++ empty_window.py
@@ -4,7 +4,7 @@ import pygame
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 
 pygame.init()
-
+clock = pygame.time.Clock()
 exit_the_game = False
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption(title="Pysteroids ☄")
@@ -18,18 +18,25 @@ ship_surf = pygame.image.load('./graphics/ship.png').convert_alpha()
 font = pygame.font.Font('./graphics/subatomic.ttf', size=50)
 text_surf = font.render('Space', True, (255, 255, 255))
 
+ship_y = 500
+ship_x = 300
+
 while True:
     # Handle Input
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            pass
+
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
     # Update the screen
+    clock.tick(60)
     display_surface.fill((12, 2, 26))
     display_surface.blit(background_surf, (0, 0))
-    display_surface.blit(text_surf, (500, 200))
-    display_surface.blit(ship_surf, (300, 500))
-
+    display_surface.blit(text_surf, (((WINDOW_WIDTH/2) - (text_surf.get_width()/2)), WINDOW_HEIGHT - 100))
+    display_surface.blit(ship_surf, (ship_x, ship_y))
+    ship_y -= 10
     # Apply the changes (end of rendering)
     pygame.display.update()
