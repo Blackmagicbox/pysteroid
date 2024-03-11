89e40952bbf0f9539561925c02d2d6ffcd34eb9c
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-03-11 21:37:21 +0100

    FEAT: Rect for displacing sprite

--- empty_window.py
+++ empty_window.py
@@ -13,13 +13,11 @@ pygame.display.set_caption(title="Pysteroids ☄")
 background_surf = pygame.image.load('./graphics/background.png').convert_alpha()
 background_surf.fill((255, 255, 255, 180), None, pygame.BLEND_RGBA_MULT)
 ship_surf = pygame.image.load('./graphics/ship.png').convert_alpha()
+ship_rec = ship_surf.get_rect(center=(640, 360))
 
 # Importing text
 font = pygame.font.Font('./graphics/subatomic.ttf', size=50)
 text_surf = font.render('Space', True, (255, 255, 255))
-
-ship_y = 500
-ship_x = 300
 
 while True:
     # Handle Input
@@ -35,8 +33,8 @@ while True:
     clock.tick(60)
     display_surface.fill((12, 2, 26))
     display_surface.blit(background_surf, (0, 0))
-    display_surface.blit(text_surf, (((WINDOW_WIDTH/2) - (text_surf.get_width()/2)), WINDOW_HEIGHT - 100))
-    display_surface.blit(ship_surf, (ship_x, ship_y))
-    ship_y -= 10
+    display_surface.blit(text_surf, (((WINDOW_WIDTH / 2) - (text_surf.get_width() / 2)), WINDOW_HEIGHT - 100))
+    ship_rec.bottom -= 4
+    display_surface.blit(ship_surf, ship_rec)
     # Apply the changes (end of rendering)
     pygame.display.update()
